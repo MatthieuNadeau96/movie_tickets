bd5bca6c0bcea244ae83be6bffbe0bec23b25c05
Matthieu Nadeau <matthieunadeau96@gmail.com>
2020-07-03 20:45:25 -0400

    Added more UI to Carousel selections

--- lib/screens/selection_screen.dart
+++ lib/screens/selection_screen.dart
@@ -1,5 +1,6 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:movie_tickets/bloc/get_now_playing_movies_bloc.dart';
 import 'package:movie_tickets/model/movie.dart';
 import 'package:movie_tickets/model/movie_response.dart';
@@ -63,61 +64,144 @@ class _SelectionScreenState extends State<SelectionScreen> {
       ),
     );
   }
-}
 
-Widget _buildNowPlayingWidget(MovieResponse data) {
-  List<Movie> movies = data.movies;
-  if (movies.length == 0) {
-    return Container(
-      child: Center(
-        child: Text('No Movies'),
-      ),
-    );
-  } else
-    return Scaffold(
-      backgroundColor: Colors.grey[700],
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            child: Column(
-              children: [
-                Container(
-                    child: CarouselSlider(
-                  options: CarouselOptions(height: 400.0),
-                  items: movies.map((i) {
-                    return Builder(
-                      builder: (BuildContext context) {
-                        return Container(
-                          child: Container(
+  Widget _buildNowPlayingWidget(MovieResponse data) {
+    final Size size = MediaQuery.of(context).size;
+    List<Movie> movies = data.movies;
+    if (movies.length == 0) {
+      return Container(
+        child: Center(
+          child: Text('No Movies'),
+        ),
+      );
+    } else
+      return Scaffold(
+        backgroundColor: Colors.grey[700],
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Container(
+              child: CarouselSlider(
+                options: CarouselOptions(
+                  height: size.height * 0.75,
+                  enlargeCenterPage: true,
+                ),
+                items: movies.map((movie) {
+                  return Builder(
+                    builder: (BuildContext context) {
+                      return Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(20),
+                            topRight: Radius.circular(20),
+                          ),
+                        ),
+                        padding: EdgeInsets.only(
+                          bottom: 30,
+                          left: 30,
+                          right: 30,
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
                             // Image container
-                            width: MediaQuery.of(context).size.width,
-                            margin: EdgeInsets.symmetric(horizontal: 5.0),
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.only(
-                                topLeft: Radius.circular(20),
-                                topRight: Radius.circular(20),
-                              ),
-                              color: Colors.white,
-                              shape: BoxShape.rectangle,
-                              image: DecorationImage(
-                                image: NetworkImage(
-                                  'https://image.tmdb.org/t/p/original/' +
-                                      i.backPoster,
+                            Expanded(
+                              child: Container(
+                                margin: EdgeInsets.symmetric(
+                                  horizontal: 15,
+                                  vertical: 30,
                                 ),
-                                fit: BoxFit.cover,
+                                width: size.width,
+                                // margin: EdgeInsets.symmetric(horizontal: 5.0),
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(20),
+                                  ),
+                                  color: Colors.white,
+                                  shape: BoxShape.rectangle,
+                                  image: DecorationImage(
+                                    image: NetworkImage(
+                                      'https://image.tmdb.org/t/p/original/' +
+                                          movie.backPoster,
+                                    ),
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
                               ),
                             ),
-                          ),
-                        );
-                      },
-                    );
-                  }).toList(),
-                )),
-              ],
+                            Text(
+                              movie.title,
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                fontSize: 28,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            SizedBox(height: 15),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  movie.rating.toString(),
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                                ),
+                                SizedBox(width: 5),
+                                RatingBar(
+                                  itemSize: 15,
+                                  initialRating: movie.rating / 2,
+                                  minRating: 1,
+                                  direction: Axis.horizontal,
+                                  allowHalfRating: true,
+                                  itemCount: 5,
+                                  itemPadding:
+                                      EdgeInsets.symmetric(horizontal: 2),
+                                  itemBuilder: (context, _) => Icon(
+                                    Icons.star,
+                                    color: Colors.yellow[800],
+                                  ),
+                                  onRatingUpdate: (rating) {
+                                    print(rating);
+                                  },
+                                ),
+                              ],
+                            ),
+                            // SizedBox(height: 5),
+                            IconButton(
+                              icon: Icon(
+                                Icons.more_horiz,
+                                color: Colors.grey[700],
+                              ),
+                              onPressed: () {},
+                            ),
+                            SizedBox(height: 5),
+                            MaterialButton(
+                              colorBrightness: Brightness.dark,
+                              color: Colors.grey[700],
+                              minWidth: size.width,
+                              padding: EdgeInsets.symmetric(vertical: 20),
+                              child: Text(
+                                'BUY TICKET',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15,
+                                ),
+                              ),
+                              onPressed: () {},
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  );
+                }).toList(),
+              ),
             ),
-          ),
-        ],
-      ),
-    );
+          ],
+        ),
+      );
+  }
 }
